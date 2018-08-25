d32281e3b0af1007b1999d2b5b38089fed9eeef1
Tsubasa Yoshida <tbs_chako@yahoo.co.jp>
2018-08-25 21:39:52 +0900

    ①コールド条件修正②サヨナラコールド条件修正③静的文言追加④日本語サイト明示

--- app/models/game_info.rb
+++ app/models/game_info.rb
@@ -21,7 +21,7 @@ class GameInfo < ApplicationRecord
         x = ""
         if (self[:bottom9].to_i > 0 || self[:bottom10].to_i > 0 || self[:bottom11].to_i > 0 || self[:bottom12].to_i > 0) && self[:updatable] && sum_bottom > sum_top
             x = "x"
-        elsif ((self[:bottom7].to_i > 0 && !self[:top8]) || (self[:bottom8].to_i > 0 && !self[:top9])) && self[:updatable]
+        elsif ((self[:bottom5].to_i > 0 && !self[:top6]) || (self[:bottom6].to_i > 0 && !self[:top7]) || (self[:bottom7].to_i > 0 && !self[:top8]) || (self[:bottom8].to_i > 0 && !self[:top9])) && self[:updatable]
             x = "x"
         end
         return x
@@ -31,6 +31,24 @@ class GameInfo < ApplicationRecord
         gameset_msg = "（試合中）"
         if self[:updatable]
             gameset_msg = "（試合終了）"
+        end
+        return gameset_msg
+    end
+
+    def get_gameset_msg_show(sum_top,sum_bottom)
+        gameset_msg = ""
+        if self[:updatable]
+            if !self[:top6]
+                gameset_msg = "（5回コールド）"
+            elsif !self[:top7]
+                gameset_msg = "（6回コールド）"
+            elsif !self[:top8]
+                gameset_msg = "（7回コールド）"
+            elsif !self[:top9]
+                gameset_msg = "（8回コールド）"
+            elsif sum_top == sum_bottom
+                gameset_msg = "（引き分け）"
+            end
         end
         return gameset_msg
     end
